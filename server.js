e79b62e30dddae711998eb6fc7eaa625aabb82e2
davcabbur <davcabbur@alu.edu.gva.es>
2025-12-11 02:33:12 +0100

    Update

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.listen(PORT, '0.0.0.0', (err) => {
     console.error('❌ Error iniciando servidor:', err);
     return;
   }
+  // Mensaje de inicio
   console.log('=================================');
   console.log('✅ SERVIDOR INICIADO CORRECTAMENTE');
   console.log(`📍 Puerto: ${PORT}`);
